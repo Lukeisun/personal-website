1d2026283bccef94ebbe331f0247650064048b00
Adin Lukas <Lukeisun57@gmail.com>
2022-05-23 10:37:21 -0400

    Better mobile functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import './App.css';
 import GHDrawn from './svg/ghdrawn.svg'
 import LI from './svg/linkedin.svg'
+import pdf from './svg/pdf.svg'
+import latexLogo from './svg/LaTeX_logo.svg'
 import email from './svg/email.svg'
 import me from './images/me.jpg'
 import resume from './resume/ALD_Resume.pdf'
 import latexResume from './resume/resume.zip'
-import pdf from './svg/pdf.svg'
-import latexLogo from './svg/LaTeX_logo.svg'
 import React, { useState, useEffect } from 'react';
 function App() {
   const [isLoading, setLoading] = useState(true);
@@ -39,8 +39,50 @@ function App() {
 }
 function Projects(){
   return (
-    <div>
+    <div className="sectionContainer">
       <h1 className='Name'>Projects</h1>
+        <SeniorDesign />
+        <LargeProject />
+        <Holobot />
+    </div>
+  );
+}
+function SeniorDesign() {
+  return (
+    <div>
+      <h2 className='Project'>Senior Design Capstone</h2>
+      <p className='projectDescription'>
+        Production program for a company that tracks their manufacturing process.
+        Designed with a microservices architecture.
+      </p>
+    </div>
+  );
+}
+
+function LargeProject()
+{
+  return (
+    <div>
+      <a href="https://github.com/Group14-COP4331/large-project">
+        <h2 className='Project'>Dungeon Run</h2>
+      </a>
+      <p className='projectDescription'>
+        A runner-esque game (think jetpack joyride) where I handled a large portion of the backend using node.js . Implementing
+        CRUD operations with MongoDB.
+      </p>
+    </div>
+  );
+}
+function Holobot(){
+  return(
+    <div>
+      <a href="https://github.com/Lukeisun/HoloBot">
+        <h2 className='Project'>HoloBot</h2>
+      </a>
+      <p className='projectDescription'>
+        Discord bot that scrapes YouTube pages that will send a message in a discord channel notifying
+        users that a creator is live. Created in Python and deployed using Heroku .
+      </p>
     </div>
   );
 }
@@ -48,7 +90,7 @@ function Resume(){
   return (
   <div>
     <h1 className='Name'>My Resume</h1>
-    <embed src={resume} width="80%" height="900vh" ></embed>
+    <embed className="resume" src={resume} width="80%" height="900vh" ></embed>
     <br></br>
     <a href={resume} download>
       <img src={pdf} className="icon"></img>
